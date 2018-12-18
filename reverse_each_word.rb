0b22a9f5e96a7383b821f10dc8d17cee89845ff9
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-18 19:35:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,19 @@
+# def reverse_each_word(str)
+#   arr=str.split(" ")
+#   newStr=[]
+#   arr.each do |word|
+#     word.reverse!
+#   end
+#   arr.join(" ")
+# end
+
 def reverse_each_word(str)
   arr=str.split(" ")
   newStr=[]
-  arr.each do |word|
+  arr.collect do |word|
     word.reverse!
   end
   arr.join(" ")
+  
+  
 end
